06c4fd954f7d024ecb93d2b1243472f7e0f02dd5
shawn yu <shawn.yu5@hotmail.com>
2022-08-17 22:02:47 -0400

    fix: all data in linked list should be comparable

--- linked_list/linked_list/linked_list.go
+++ linked_list/linked_list/linked_list.go
@@ -1,16 +1,27 @@
 package linkedlist
 
-type linkedList[T any] struct {
+type linkedList[T comparable] struct {
 	front *node[T]
 	back  *node[T]
 }
 
-type node[T any] struct {
-	data T
+type node[T comparable] struct {
 	next *node[T]
 	prev *node[T]
+	data T
 }
 
+type Const_iterator[T comparable] struct {
+	MyList  *linkedList[T]
+	Current *node[T]
+}
+
+type Iterator[T comparable] struct {
+	Const_iterator[T]
+}
+
+// NewList constructs a new list with sentinel nodes
+// returns a new linkedlist
 func (linkedList[T]) NewList() linkedList[T] {
 	list := linkedList[T]{front: &node[T]{}, back: &node[T]{}}
 	return list
