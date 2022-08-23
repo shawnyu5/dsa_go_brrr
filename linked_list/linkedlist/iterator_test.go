b4b9c481abb27886b0852378adad200493a7dd26
shawn yu <shawn.yu5@hotmail.com>
2022-08-23 12:49:43 -0400

    test(iterator): tests in seperate `_test` package

--- linked_list/linkedlist/iterator_test.go
+++ linked_list/linkedlist/iterator_test.go
@@ -1,22 +1,33 @@
-package linkedlist
+package linkedlist_test
 
 import (
+	"github.com/shawnyu5/linked_list/linkedlist"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Iterator", func() {
 	It("get() should return the data within the iterator", func() {
-		n := node[int]{data: 10}
-		it := &Iterator[int]{current: &n}
+		// n := node[int]{data: 10}
+		list := linkedlist.NewList[int]()
+		it := list.It.Begin()
+		list.Insert(&it, 10)
 		data := it.Get()
 
 		Expect(data).To(Equal(10))
 	})
 
 	It("should increment the iterator to the next node", func() {
-		n := node[int]{data: 10, next: &node[int]{data: 20, next: &node[int]{data: 30}}, prev: &node[int]{data: 0}}
-		it := &Iterator[int]{current: &n}
+		list := linkedlist.NewList[int]()
+		it := list.It.Begin()
+		list.Insert(&it, 10)
+		it.Increment()
+		list.Insert(&it, 20)
+		it.Increment()
+		list.Insert(&it, 30)
+
+		it = list.It.Begin()
 		Expect(it.Get()).To(Equal(10))
 		it.Increment()
 		Expect(it.Get()).To(Equal(20))
@@ -25,24 +36,22 @@ var _ = Describe("Iterator", func() {
 	})
 
 	Context("(c)begin()", func() {
-		It("should return the begining of the list - iterator", func() {
-			n := node[int]{data: 10, next: &node[int]{data: 20, next: &node[int]{data: 30}}, prev: &node[int]{data: 0}}
+		It("Itertor begin should return the begining of list", func() {
 			// construct a new list with sentinel nodes
-			list := NewList[int]()
+			list := linkedlist.NewList[int]()
 
-			it := newIterator(&n, &list)
+			it := list.It.Begin()
 			// begin and end should point to the same sentinel node
 			Expect(it.Begin()).To(Equal(it.End()))
 		})
 
-		It("should return the begining of the list - const iterator", func() {
-			n := node[int]{data: 10, next: &node[int]{data: 20, next: &node[int]{data: 30}}, prev: &node[int]{data: 0}}
+		It("cbegin() should return the begining of list", func() {
 			// construct a new list with sentinel nodes
-			list := NewList[int]()
+			list := linkedlist.NewList[int]()
 
-			it := newConstIterator(&n, &list)
+			cIt := list.Cit.CBegin()
 			// begin and end should point to the same sentinel node
-			Expect(it.CBegin()).To(Equal(it.CEnd()))
+			Expect(cIt.CBegin()).To(Equal(cIt.CEnd()))
 		})
 	})
 })
