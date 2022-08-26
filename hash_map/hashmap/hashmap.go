d04b4c3d69cb3eea6decf8cfb1c9186a5bbd7900
shawn yu <shawn.yu5@hotmail.com>
2022-08-25 21:59:37 -0400

    feat: hash function
    hashes a string

--- hash_map/hashmap/hashmap.go
+++ hash_map/hashmap/hashmap.go
@@ -1,1 +1,13 @@
 package hashmap
+
+import (
+	"crypto/sha256"
+	"hash"
+)
+
+// Hash takes a string and returns a hash object from it
+func Hash(key string) hash.Hash {
+	hash := sha256.New()
+	hash.Write([]byte(key))
+	return hash
+}
--- hash_map/hashmap/hashmap_test.go
+++ hash_map/hashmap/hashmap_test.go
@@ -1,1 +1,17 @@
 package hashmap_test
+
+import (
+	"github.com/shawnyu5/hash_map/hashmap"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Hashmap", func() {
+	Context("Hash()", func() {
+		It("should return a hash object from a string", func() {
+			hash := hashmap.Hash("hello")
+			Expect(hash).ToNot(BeNil())
+		})
+	})
+})
