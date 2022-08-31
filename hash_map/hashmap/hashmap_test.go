d1b8175aaa7693d36f214c4ef47d01d9e14505d6
shawn yu <shawn.yu5@hotmail.com>
2022-08-31 10:27:33 -0400

    test: `createData()`
    reads a text file to create strings of keys for the hashmap

--- hash_map/hashmap/hashmap_test.go
+++ hash_map/hashmap/hashmap_test.go
@@ -1,11 +1,36 @@
 package hashmap_test
 
 import (
+	"bufio"
+	"bytes"
+	"os"
+
 	"github.com/shawnyu5/hash_map/hashmap"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+// createData reads data.txt and returns a slice of strings to act as keys for testing
+func createData() []string {
+	data, err := os.ReadFile("../data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	bytesReader := bytes.NewReader(data)
+	bufReader := bufio.NewReader(bytesReader)
+	arr := []string{}
+
+	for {
+		value1, _, _ := bufReader.ReadLine()
+		if value1 == nil {
+			break
+		}
+		arr = append(arr, string(value1))
+	}
+	return arr
+}
 
 var _ = Describe("LPTable", func() {
 	Context("Hash()", func() {
